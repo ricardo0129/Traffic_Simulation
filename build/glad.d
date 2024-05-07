71499cc4b17e8da2b0ab00ff2eccbc06fc84544b
ricardo0129 <ruizr274@gmail.com>
2024-05-06 21:46:50 -0400

    refractor

--- build/glad.d
+++
@@ -1,6 +0,0 @@
-build/glad.o: glad.cpp include/glad/glad.h \
-  include/glad/../KHR/khrplatform.h
-
-include/glad/glad.h:
-
-include/glad/../KHR/khrplatform.h:
--- main.cpp
+++ main.cpp
@@ -9,28 +9,33 @@
 
 struct Camera {
     glm::vec3 camPos, camFront, camUp;
+    float sensitivity = 0.1f;
+    Camera() {
+        camPos = glm::vec3(0.0f, 0.0f,  3.0f);
+        camFront = glm::vec3(0.0f, 0.0f, -1.0f);
+        camUp    = glm::vec3(0.0f, 1.0f,  0.0f);
+    }
+    void move(glm::vec2 dir) {
+        camPos += (camFront * dir[0] + glm::normalize(glm::cross(camFront, camUp)) * dir[1]) * sensitivity;
+    }
 };
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
 
-glm::vec3 cameraPos;
-glm::vec3 cameraFront;
-glm::vec3 cameraUp;
+Camera c;
 void processMove(GLFWwindow* window) {
     if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
-        cameraPos += 0.1f * cameraFront;
+        c.move(glm::vec2(1, 0));
     }
     if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
-        cameraPos -= 0.1f * cameraFront;
+        c.move(glm::vec2(-1, 0));
     }
     if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS) {
-        glm::vec3 cameraRight = glm::normalize(glm::cross(cameraFront, cameraUp));
-        cameraPos -= 0.1f * cameraRight;
+        c.move(glm::vec2(0, -1));
     }
     if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS) {
-        glm::vec3 cameraRight = glm::normalize(glm::cross(cameraFront, cameraUp));
-        cameraPos += 0.1f * cameraRight;
+        c.move(glm::vec2(0, 1));
     }
 }
 float lastX, lastY;
@@ -63,38 +68,12 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos) {
     direction.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
     direction.y = sin(glm::radians(pitch));
     direction.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
-    cameraFront = glm::normalize(direction);
+    c.camFront = glm::normalize(direction);
 }  
 
 // settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
-
-/*
-const char *vertexShaderSource = "#version 330 core\n"
-    "layout (location = 0) in vec3 aPos;\n"
-    "void main()\n"
-    "{\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-    "}\0";
-    */
-
-const char *vertexShaderSource = "#version 330 core\n"
-    "layout (location = 0) in vec3 aPos;\n"
-    "uniform mat4 projection;\n"
-    "uniform mat4 view;\n"
-    "uniform mat4 model;\n"
-    "void main()\n"
-    "{\n"
-    "gl_Position = projection * view * model * vec4(aPos, 1.0f);\n"
-    "}\0";
-
-const char *fragmentShaderSource = "#version 330 core\n"
-    "out vec4 FragColor;\n"
-    "void main()\n"
-    "{\n"
-    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-    "}\0";
 
 int main() {
 
@@ -112,7 +91,6 @@ int main() {
 
 
     // glfw window creation
-    // --------------------
     GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
     if (window == NULL) {
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -126,7 +104,6 @@ int main() {
 
 
     // glad: load all OpenGL function pointers
-    // ---------------------------------------
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
@@ -135,14 +112,6 @@ int main() {
 
     Shader st("vertex.vert", "fragment.frag");
 
-    // set up vertex data (and buffer(s)) and configure vertex attributes
-    // ------------------------------------------------------------------
-    /*
-    float vertices[] = {
-        -0.5f, -0.5f, 0.0f, // left  
-         0.5f, -0.5f, 0.0f, // right 
-         0.0f,  0.5f, 0.0f  // top   
-    };*/ 
     float vertices[] = {
         -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
          0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
@@ -198,6 +167,7 @@ int main() {
         glm::vec3( 1.5f,  0.2f, -1.5f),
         glm::vec3(-1.3f,  1.0f, -1.5f)
     };
+
     unsigned int VBO, VAO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
@@ -210,38 +180,34 @@ int main() {
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
-    // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
     glBindBuffer(GL_ARRAY_BUFFER, 0); 
-
-    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
-    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
     glBindVertexArray(0); 
-
-
     // uncomment this call to draw in wireframe polygons.
     //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
     // render loop
-    // -----------
-    // p
     float deg = 0;
+    int frames = 0;
+    auto start = std::chrono::high_resolution_clock::now();
 
-    cameraPos   = glm::vec3(0.0f, 0.0f,  3.0f);
-    cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
-    cameraUp    = glm::vec3(0.0f, 1.0f,  0.0f);
     while (!glfwWindowShouldClose(window)) {
         // input
-        // -----
         processInput(window);
         processMove(window);
-
+        frames += 1;
+        auto end = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> elapsed_seconds = end - start;
+        if( elapsed_seconds.count() > 1.0 ) {
+            std::cout<<"FRAMES PER SEC "<<frames<<std::endl;
+            start = std::chrono::high_resolution_clock::now();
+            frames = 0;
+        }
         // render
         // ------
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         // draw our first triangle
-        //glUseProgram(shaderProgram);
         glUseProgram(st.ID);
 
 
@@ -253,18 +219,11 @@ int main() {
         glm::mat4 model         = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
         glm::mat4 projection    = glm::mat4(1.0f);
         model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.0f));
-        //glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);  
-        glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
-        glm::vec3 cameraDirection = glm::normalize(cameraPos - cameraTarget);
-        glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f); 
-        glm::vec3 cameraRight = glm::normalize(glm::cross(up, cameraDirection));
-        glm::vec3 cameraUp = glm::cross(cameraDirection, cameraRight);
         const float radius = 10.0f;
         float camX = sin(glfwGetTime()) * radius;
         float camZ = cos(glfwGetTime()) * radius;
         glm::mat4 view;
-        //view = glm::lookAt(glm::vec3(camX, 0.0, camZ), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 1.0, 0.0));
-        view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+        view = glm::lookAt(c.camPos, c.camPos + c.camFront, c.camUp);
 
         projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
         // retrieve the matrix uniform locations
@@ -279,11 +238,8 @@ int main() {
             glm::mat4 model = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
             model = glm::translate(model, cubePositions[i]);
             float angle = 20.0f * i;
-            //model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
             model = glm::rotate(model, (i+1) * (float)glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.0f));
-            //ourShader.setMat4("model", model);
             glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-
             glDrawArrays(GL_TRIANGLES, 0, 36);
         }
         // glBindVertexArray(0); // no need to unbind it every time 
@@ -300,23 +256,17 @@ int main() {
     glDeleteBuffers(1, &VBO);
     glDeleteProgram(st.ID);
 
-    // glfw: terminate, clearing all previously allocated GLFW resources.
-    // ------------------------------------------------------------------
     glfwTerminate();
     return 0;
 }
 
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
-// ---------------------------------------------------------------------------------------------------------
 void processInput(GLFWwindow *window) {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
-// ---------------------------------------------------------------------------------------------
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
-    // make sure the viewport matches the new window dimensions; note that width and 
-    // height will be significantly larger than specified on retina displays.
     glViewport(0, 0, width, height);
 }
